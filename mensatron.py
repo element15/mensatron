4224d4fe35ba5b172c79f705577a692627326af4
Christian Moomaw <element15@phosfor.us>
2019-06-04 11:15:02 +0200

    Add mensatron.py

---
+++ mensatron.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+# The one and only @mensatron. This script is intended to be invoked as a `cron`
+# job once an hour, more or less on the hour.
+# Written by Christian Moomaw
+# Last revised 2019-06-01
+
+def get_api():
+	# Only setup for twitter if necessary
+	from secrets import *
+	import tweepy
+
+	auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
+	auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
+	api = tweepy.API(auth)
+	return api
+
+def tweet(api):
+	tweet_list = ( # (<cumulative weight>, <tweet text>)
+		(6, "Ciao, dimmi."),
+		(14, "Dimmi."),
+		(16, "Buon appetito."),
+	)
+	index = randrange(tweet_list[-1][0])
+	for i in tweet_list:
+		if index < i[0]:
+			api.update_status(i[1])
+			return
+
+def tweet_burst():
+	# Units for the following parameters are in minutes
+	tweet_spacing_mu = 15
+	tweet_spacing_sigma = 10
+	max_burst_length = 30
+
+	from datetime import datetime
+	from random import (randrange, gauss)
+	from time import sleep
+
+	api = get_api()
+	init_hour = datetime.now().hour
+	target_minute_init = randrange(60)
+	target_minute = target_minute_init
+	while (datetime.now().hour == init_hour and target_minute < 60
+		and (target_minute - target_minute_init) < max_burst_length):
+		sleep((target_minute - datetime.now().minute) * 60)
+		tweet(api)
+		target_minute += max(int(gauss(
+			tweet_spacing_mu, tweet_spacing_sigma)), 0)
+
+from random import random
+hourly_chance = 0.02
+if random() < hourly_chance:
+	tweet_burst()
